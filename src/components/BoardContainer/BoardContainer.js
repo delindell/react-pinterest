d40a68bd76b7df866543744b0cb7376261b3e7eb
delindell <daviselindell@gmail.com>
2020-05-23 09:48:21 -0500

    can delete all boards

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -6,6 +6,7 @@ import boardsData from '../../helpers/data/boardsData';
 import authData from '../../helpers/data/authData';
 
 import Board from '../Board/Board';
+import smash from '../../helpers/data/smash';
 
 class BoardContainer extends React.Component {
   static propTypes = {
@@ -16,16 +17,26 @@ class BoardContainer extends React.Component {
     boards: [],
   }
 
-  componentDidMount() {
+  getAllBoards = () => {
     boardsData.getBoardsByUid(authData.getUid())
       .then((boards) => this.setState({ boards }))
       .catch((err) => console.error('unable to get boards', err));
   }
 
+  componentDidMount() {
+    this.getAllBoards();
+  }
+
+  removeBoard = (boardId) => {
+    smash.completelyRemoveBoard(boardId)
+      .then(() => this.getAllBoards())
+      .catch((err) => console.error('coulndt delete full board', err));
+  }
+
   render() {
     const { boards } = this.state;
     const { setSingleBoard } = this.props;
-    const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard}/>);
+    const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} removeBoard={this.removeBoard} />);
 
     return (
       <div className="BoardContainer">
