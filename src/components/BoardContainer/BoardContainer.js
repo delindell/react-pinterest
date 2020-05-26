f32e4483535ed9621ca7071b32522f5df333cf4d
delindell <daviselindell@gmail.com>
2020-05-26 18:19:52 -0500

    gots a button

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -7,6 +7,7 @@ import authData from '../../helpers/data/authData';
 
 import Board from '../Board/Board';
 import smash from '../../helpers/data/smash';
+import BoardForm from '../BoardForm/BoardForm';
 
 class BoardContainer extends React.Component {
   static propTypes = {
@@ -15,6 +16,7 @@ class BoardContainer extends React.Component {
 
   state = {
     boards: [],
+    formOpen: false,
   }
 
   getAllBoards = () => {
@@ -34,13 +36,15 @@ class BoardContainer extends React.Component {
   }
 
   render() {
-    const { boards } = this.state;
+    const { boards, formOpen } = this.state;
     const { setSingleBoard } = this.props;
     const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} removeBoard={this.removeBoard} />);
 
     return (
       <div className="BoardContainer">
         <h2>Boards</h2>
+        <button className="btn btn-warning" onClick={() => this.setState({ formOpen: true })}>Make A Damn Barwd</button>
+        { formOpen ? <BoardForm /> : '' }
         <div className="d-flex flex-wrap">
           {makeBoards}
         </div>
